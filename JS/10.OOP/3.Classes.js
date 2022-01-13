51d869bf43820171b49e834266446cc63a393aa1
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-01-13 15:43:52 +0100

    OOP

---
+++ JS/10.OOP/3.Classes.js
@@ -0,0 +1,72 @@
+class Book{
+
+    constructor(title, author, year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.revised = false;
+    }
+
+    summary(){
+        return `${this.title} was written by ${this.author}`;
+    }
+
+}
+
+const book1 = new Book('C#', 'Kalle', 2020);
+
+// const book2 = new Book(2, {}, []);
+// console.log(book2);
+// book2.title = [1,2,3];
+// console.log(book2.title);
+// book2.title = "Kalle";
+// book2.title = {firstName : 'Kalle'};
+// console.log(book2);
+
+
+// console.log(book1);
+// console.log(book1.summary());
+// console.log(book1.title);
+// console.log(typeof book1.author);
+// console.log(typeof book1.year);
+// console.log(typeof book1);
+// console.log(typeof book1.revised);
+
+
+class Person{
+
+    #firstName;
+    #lastName;
+
+    get firstName(){
+        return  this.#firstName;
+    }
+
+    set fullName(value){
+        const parts = value.split(' ');
+        this.#firstName = parts[0];
+        this.#lastName = parts[1];
+    }
+
+    constructor(firstName, lastName){
+        this.#firstName = firstName;
+        this.#lastName = lastName;
+    }
+
+    #privateFunc(){
+        console.log("private");
+    }
+
+    GetFullNameFunktion(){
+        this.#privateFunc();
+        return `${this.#firstName} ${this.#lastName}`;
+    }
+
+}
+
+let person = new Person('Kalle', 'Anka');
+// console.log(person.GetFullNameFunktion());
+// console.log(person.firstName);
+// person.fullName = 'Anders Karlsson';
+// console.log(person);
+// console.log(person.privateFunc());
